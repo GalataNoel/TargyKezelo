eb551fdf5f41e7513ce042f715ae84b9b096ad69
Galata Noel <galatanoel@gmail.com>
2022-12-08 10:59:36 +0100

    fixed Felvetel.java added db connetion

--- src/main/java/hu/nye/com/Main.java
+++ src/main/java/hu/nye/com/Main.java
@@ -11,22 +11,6 @@ import java.sql.*;
  */
 public class Main extends Felvetel {
     public static void main(String[] args) throws IOException, SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:h2:tcp://localhost/./jdbc-test","sa","password");
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("Select * FROM USERS");
-
-        while(resultSet.next()){
-            String nev = resultSet.getString("targyNev");
-            String kredit = resultSet.getString("targyKredit");
-            String ido = resultSet.getString("targyIdo");
-
-            Targy targy1 = new Targy(targyNev,targyKredit,targyIdo);
-            System.out.println(targy1);
-        }
-        resultSet.close();
-        statement.close();
-        connection.close();
-
         Felvetel felvetel = new Felvetel();
         felvetel.targyHozzaadas();
 
